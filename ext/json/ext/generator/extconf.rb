597ec1866c22d9cff4ad1fc6a0b032051d15b7a2
Florian Frank <flori@ping.de>
2009-10-18 10:33:42 +0200

    pure fallback
    attempt to create a fake Makefile if a build system couldn't be found.
    This should make it possible for the JSON gem to fall back on pure later
    while pretending to have successfully built the extension to the rest of
    the infrastructure during the gem installation.

--- ext/json/ext/generator/extconf.rb
+++ ext/json/ext/generator/extconf.rb
@@ -1,11 +1,40 @@
 require 'mkmf'
 require 'rbconfig'
+include Config
 
-if CONFIG['CC'] =~ /gcc/
-  $CFLAGS += ' -Wall'
-  #$CFLAGS += ' -O0 -ggdb'
+CC = system(cc = CONFIG['CC'], '-v') && cc
+if CC
+	if CC =~ /gcc/
+		$CFLAGS += ' -Wall'
+		#$CFLAGS += ' -O0 -ggdb'
+	end
+
+  if (method(:have_header) rescue nil)
+    have_header("ruby/st.h") || have_header("st.h")
+    have_header("ruby/encoding.h")
+  end
+
+	warn "Build system found: Installing the EXT variant of JSON."
+  create_makefile 'generator'
+else
+	warn "Build system not found: Installing the PURE variant of JSON instead."
+	File.open('Makefile', 'w') do |mf|
+		mf.puts <<EOT
+all:
+	ruby -e 'File.open("generator.#{CONFIG['DLEXT']}", "wb") {}'
+
+static:
+
+clean:
+
+install:
+
+site-install:
+
+distclean:
+	ruby -rfileutils -e 'FileUtils.rm_f ARGV'
+realclean: distclean
+EOT
+	end
 end
 
-have_header("ruby/st.h") || have_header("st.h")
-have_header("ruby/encoding.h")
-create_makefile 'generator'
